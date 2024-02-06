b19aa16546c53209766bbefcb94ee43a2d1238a3
david arenas <darenas@cesde.net>
2024-02-06 11:37:00 -0500

    fin

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,6 @@
 producto = "computador"
 cantidad = 5
-precio = 5000
-
+precio = 1000
 descuento = 0.25
 iva = 0.19
 precio_descuento = precio-(precio * descuento)
@@ -9,3 +8,11 @@ total_con_descuento = precio_descuento * cantidad
 total_con_iva = total_con_descuento + (total_con_descuento * iva)
 
 total = total_con_descuento + total_con_iva
+
+print(f"""
+            producto {producto}
+            cantidad {cantidad}
+            precio {precio}
+            precio con descuento {total_con_descuento}
+            precio con iva {total_con_iva}
+            neto {total}""")
