e45efef53deb79f41291fd68c38bc67fcdbc8149
david arenas <darenas@cesde.net>
2024-02-06 11:32:05 -0500

    calculos matematicos

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,3 +2,9 @@ producto = "computador"
 cantidad = 5
 precio = 5000
 
+descuento = 0.25
+iva = 0.19
+precio_descuento = precio-(precio * descuento)
+total_con_descuento = precio_descuento * cantidad
+total_con_iva = total_con_descuento + (total_con_descuento * iva)
+
